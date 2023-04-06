30133446b9477b2de8e7af5c5b13b4e62397f4bc
Nauris Dorbe <naurisdorbe@gmail.com>
2023-04-06 10:42:46 +0000

    preparing as installation package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DASS_Det_Inference',
+    version='0.0.1',
+    description='DASS_Det_Inference',
+    author='DASS_Det_Inference',
+    author_email='DASS_Det_Inference',
+    packages=find_packages()
+)
