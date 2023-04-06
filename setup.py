861ec5a2af2de0050ca78d8ab2408c641acac2d5
Nauris Dorbe <naurisdorbe@gmail.com>
2023-04-06 14:05:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dass_inference',
+    name='dass_det',
     py_modules=["dass_det"],
     version='0.0.1',
     description='DASS_Det_Inference',
